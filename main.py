f3a561cdfec1c675a6aa9ef0112d0d3328ff9b55
rene19082003 <renat.caric123@gmail.com>
2025-03-18 16:45:14 +0100

    feat: main i readme

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
+   
+   # PIS-Vjezba1
+def README.md():
+    print("Python projekt za vježbu korištenja Git sustava za verzioniranje.")
+
+if __name__ == "__README.md__":
+    README.md()
+
